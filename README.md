63175e98d035b6fa0bf341d5e4bbe671b2395d0b
Cole Manel <csmanel@gmail.com>
2024-11-20 15:08:42 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## Customize
+You can configure and customize everything according to your iwsh by replacing the default values in the [`config.js`](./config.js) file.
+
+## Run Locally
+Clone the repository.
+```bash
+git clone https://github.com/asheeeshh/ayyy.git
+```
+
+Install dependencies.
+```bash
+cd ayyy
+npm install
+``` 
+
+Make changes and run the app.
+```bash
+npm run dev
+```
+
---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import Avatar from "../components/Avatar"
+import IconBar from "../components/IconBar"
+import { config } from "../config"
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center w-screen h-screen p-6" style={{backgroundColor: `${config.bgColor}`}}>
+      <Head>
+        <title>{config.name}</title>
+        <meta name="description" content={config.description} />
+        <script async defer data-website-id="4c450443-d56a-4484-b14f-01237f9daef9" src="https://umami-brown-phi.vercel.app/umami.js"></script>
+        <link rel="icon" href={(process.env.NEXT_PUBLIC_CLOUDIMG_TOKEN) ? `https://${process.env.NEXT_PUBLIC_CLOUDIMG_TOKEN}.cloudimg.io/${config.avatar}?radius=500` : `${config.avatar}`} />
+      </Head>
+      <div className="flex flex-col justify-center align-center w-full lg:w-1/2 md:w-1/3 h-[80%] lg:h-1/2 md:h-1/2 items-center">
+        <div className="w-full h-full flex flex-col justify-center items-center">
+          <Avatar />
+          <h1 className="text-center text-xl font-[600] mt-3" style={{color: `${config.textColor}`}}>{config.name}</h1>
+          <h1 className="text-[${config.textColor}] text-center text-md font-normal mt-5" style={{color: `${config.textColor}`}}>{config.description}</h1>
+          <div className="w-full h-1/4 mt-5 lg:mt-3 md:mt-3">
+            <IconBar />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
